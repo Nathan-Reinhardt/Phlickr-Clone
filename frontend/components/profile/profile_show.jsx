9af1bf02c8fb2efff01b166004bcd670a6450f2d
Ticonderago <nreinhardt1234@gmail.com>
2020-03-25 15:36:21 -0700

    remade the profile show component to be a class instead of a function

--- frontend/components/profile/profile_show.jsx
+++ frontend/components/profile/profile_show.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import LogoDetailBarContainer from '../navbar/logo_bar_container';
 
-const ProfileShow = (props) => {
+class ProfileShow extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            cameraRoll: "false"
+        }
+        this.spliceEmail = this.spliceEmail.bind(this);
+        this.cameraRollButton = this.cameraRollButton.bind(this);
+    }
 
-    const spliceEmail = () => {
-        let email = props.currentUser.email;
+    spliceEmail() {
+        let email = this.props.currentUser.email;
         let result = "";
         let letter = "";
         let count = 0;
@@ -24,69 +33,86 @@ const ProfileShow = (props) => {
         }
     }
 
-    const display = props.currentUser ? (
-        <div className="profile-page-container">
-            <div className="profile-cover">
-                <div className="profile-contents">
-                    <div>
-                        <h1 className="profile-name">
-                            {props.currentUser.first_name} {props.currentUser.last_name}
-                        </h1>
-                    </div>
-                    <div className="full-content-container">
-                        <div className="email-follow-container">
-                            <h1 className="short-email">
-                                {spliceEmail()}
-                            </h1>
-                            <h1 className="followers-count">
-                                0 Followers
-                            </h1>
-                            <h1 className="white-dot">
-                                .
-                            </h1>
-                            <h1 className="following-count">
-                                0 Following
+    cameraRollButton() {
+        let currId = document.getElementById("no-camera-roll-cont").id;
+
+        if (currId === "no-camera-roll-cont") {
+            document.getElementById("no-camera-roll-cont").id = "camera-roll-cont";
+        }
+    }
+
+    componentWillMount() {
+
+    }
+
+    render() {
+        const display = this.props.currentUser ? (
+            <div className="profile-page-container">
+                <div className="profile-cover">
+                    <div className="profile-contents">
+                        <div>
+                            <h1 className="profile-name">
+                                {this.props.currentUser.first_name} {this.props.currentUser.last_name}
                             </h1>
                         </div>
-                        <div className="photo-joined-container">
-                            <h1 className="photo-upload-amount">
-                                0 Photos
-                            </h1>
-                            <h1 className="joined-date">
-                                Joined 2020
-                            </h1>
+                        <div className="full-content-container">
+                            <div className="email-follow-container">
+                                <h1 className="short-email">
+                                    {this.spliceEmail()}
+                                </h1>
+                                <h1 className="followers-count">
+                                    0 Followers
+                                </h1>
+                                <h1 className="white-dot">
+                                    .
+                                </h1>
+                                <h1 className="following-count">
+                                    0 Following
+                                </h1>
+                            </div>
+                            <div className="photo-joined-container">
+                                <h1 className="photo-upload-amount">
+                                    0 Photos
+                                </h1>
+                                <h1 className="joined-date">
+                                    Joined 2020
+                                </h1>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="logged-tabs-container">
-                <div className="profile-tabs-container">
-                    <button className="about-but">About</button>
-                    <button className="photostream-but">Photostream</button>
-                    <button className="albums-but">Albums</button>
-                    <button className="faves-but">Faves</button>
-                    <button className="gallery-but">Galleries</button>
-                    <button className="profile-groups-but">Groups</button>
-                    <button className="stats-but">Stats</button>
-                    <button className="camera-roll-but">Camera Roll</button>
+                <div className="logged-tabs-container">
+                    <div className="profile-tabs-container">
+                        <button className="about-but">About</button>
+                        <button className="photostream-but">Photostream</button>
+                        <button className="albums-but">Albums</button>
+                        <button className="faves-but">Faves</button>
+                        <button className="gallery-but">Galleries</button>
+                        <button className="profile-groups-but">Groups</button>
+                        <button className="stats-but">Stats</button>
+                        <button className="camera-roll-but" onClick={this.cameraRollButton}>Camera Roll</button>
+                    </div>
+                </div>
+                <div className="">
+                    <div id="no-camera-roll-cont">
+                        <h1 className="">I</h1>
+                        <h3 className="">am</h3>
+                        <h3 className="">jesus</h3>
+                        <input className="camera-photo-input" type="file"/>
+                    </div>
                 </div>
             </div>
-            <div className="">
-                <div id="no-camera-roll-cont">
+        ) : (
+            null
+        );
 
-                </div>
+        return (
+            <div className="profile-page-container">
+                <LogoDetailBarContainer />
+                {display}
             </div>
-        </div>
-    ) : (
-        null
-    );
-
-    return (
-        <div className="profile-page-container">
-            <LogoDetailBarContainer />
-            {display}
-        </div>
-    );
+        );
+    }
 }
 
 export default ProfileShow;
